28be08eb8d94f0a29580fc8915b939ba42bdb925
Дукенбай Урзия Әбдіхалыққызы <urziya.dukenbay@kaspi.kz>
2024-09-03 10:01:44 +0500

    configuring project for spring-test-web

---
+++ spring-test-web/src/test/java/com/spring_test_web/spring_test_web/SpringTestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_test_web.spring_test_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
